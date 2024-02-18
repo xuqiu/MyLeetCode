68005c53d8339038a389b45c84bdb8a41ef1555d
殷振南 <yzn01714396@antgroup.com>
2024-02-18 16:17:25 +0800

    solutions

--- src/com/github/xuqiu/leetcode/p_31_60/LeetCode45.java
+++ src/com/github/xuqiu/leetcode/p_31_60/LeetCode45.java
@@ -7,26 +7,26 @@ import org.junit.Test;
  * @since 2024-02-18
  */
 public class LeetCode45 {
-    int steps=0;
     public int jump(int[] nums) {
         if (nums.length == 1) return 0;
-        steps = 0;
-        j(nums.length - 1,nums);
-        return steps;
-    }
-    public void j(int to,int[] nums){
-        steps++;
-        for (int i = 0; i < nums.length; i++) {
+        int steps=0;
+        int to = nums.length - 1;
+        for (int i = 0; i <= to; i++) {
             if (i + nums[i] >= to){
                 if (i != 0){
-                    j(i,nums);
+                    to = i;
+                    i = -1;
+                    steps++;
+                }else{
+                    return steps + 1;
                 }
-                break;
             }
         }
+        return steps;
     }
     @Test
     public void test(){
+        System.out.println(jump(new int[]{0}));
         System.out.println(jump(new int[]{2,3,1,1,4}));
         System.out.println(jump(new int[]{2,3,0,1,4}));
 

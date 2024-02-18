927c9a8e555141f4e8f415dbdb5d95259b17b1d5
殷振南 <yzn01714396@antgroup.com>
2024-02-18 17:48:25 +0800

    solutions

--- src/com/github/xuqiu/leetcode/p_31_60/LeetCode45.java
+++ src/com/github/xuqiu/leetcode/p_31_60/LeetCode45.java
@@ -5,27 +5,28 @@ import org.junit.Test;
 /**
  * @author xuqiu
  * @since 2024-02-18
+ * refine to 3ms
  */
 public class LeetCode45 {
     public int jump(int[] nums) {
         if (nums.length == 1) return 0;
-        int steps=0;
-        int to = nums.length - 1;
-        for (int i = 0; i <= to; i++) {
-            if (i + nums[i] >= to){
-                if (i != 0){
-                    to = i;
-                    i = -1;
-                    steps++;
-                }else{
-                    return steps + 1;
+        if (nums[0] >= nums.length - 1) return 1;
+        int[] stepTo = new int[nums.length];
+        stepTo[0] = 0;
+        stepTo[1] = nums[0];
+        for (int step = 1; step < nums.length; step++) {
+            for (int i = stepTo[step-1]+1; i <= stepTo[step]; i++) {
+                if (i + nums[i] >= nums.length - 1) {
+                    return step + 1;
                 }
+                stepTo[step + 1] = Math.max(stepTo[step +1], i + nums[i]);
             }
         }
-        return steps;
+        return 0;
     }
     @Test
     public void test(){
+        System.out.println(jump(new int[]{1,1,1,1}));
         System.out.println(jump(new int[]{0}));
         System.out.println(jump(new int[]{2,3,1,1,4}));
         System.out.println(jump(new int[]{2,3,0,1,4}));
